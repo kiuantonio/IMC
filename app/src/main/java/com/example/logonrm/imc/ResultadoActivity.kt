c571412b0b68bf051a25e4e6c0b4956725c8af06
logonrm <logonrm@fiap.com.br>
2018-03-02 09:34:24 -0300

    Calculo do IMC funcionando

--- app/src/main/java/com/example/logonrm/imc/ResultadoActivity.kt
+++ app/src/main/java/com/example/logonrm/imc/ResultadoActivity.kt
@@ -2,11 +2,54 @@ package com.example.logonrm.imc
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.content.ContextCompat
+import com.example.logonrm.imc.extension.format
+import kotlinx.android.synthetic.main.activity_resultado.*
 
 class ResultadoActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_resultado)
+
+        if (intent != null){
+            val peso = intent.getStringExtra("Peso").toDouble()
+            val altura = intent.getStringExtra("Altura").toDouble()
+
+            val imc = peso / (altura * altura)
+
+            tvValorIMC.text = imc.format(1)
+
+            if (imc <= 18.5){
+                alteraImagem(R.drawable.abaixo)
+                tvStatusIMC.text = "Abaixo do Peso"
+            }
+            else if (imc <= 24.9){
+                alteraImagem(R.drawable.ideal)
+                tvStatusIMC.text = "Peso Ideal"
+            }
+            else if(imc <= 29.9){
+                alteraImagem(R.drawable.sobre)
+                tvStatusIMC.text = "Sobrepeso"
+            }
+            else if(imc <= 34.9){
+                alteraImagem(R.drawable.obesidade)
+                tvStatusIMC.text = "Obesidade I"
+            }
+            else if(imc <= 39.9){
+                alteraImagem(R.drawable.obesidade)
+                tvStatusIMC.text = "Obesidade II"
+            }
+            else {
+                alteraImagem(R.drawable.obesidade)
+                tvStatusIMC.text = "Obesidade III"
+            }
+        }
+    }
+
+    fun  alteraImagem(IdImagem: Int){
+        ivStatusIMC.setImageDrawable(
+                ContextCompat.getDrawable(this,
+                        IdImagem))
     }
 }
